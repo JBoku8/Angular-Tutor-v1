7d2d7786be77abc9a4ca299aee348e50b130ac47
Jano Bokuchava <m.prog@yahoo.com>
2021-02-22 20:31:02 +0400

    ng intro 1

--- intro-app/src/app/app.module.ts
+++ intro-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountComponent } from './account/account.component';
+import { Dashboard } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountComponent,
+    Dashboard
   ],
   imports: [
     BrowserModule
