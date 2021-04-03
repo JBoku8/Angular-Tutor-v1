a79ec885b217ff488fec182dac782fb18d69485e
Jano Bokuchava <https://github.com/login/oauth/authorize?state=socketId%3A50e1b855-ef2e-41fe-ac81-bf197b50c84e%3Baction%3Alogin%3BinApp%3Atrue%3BneedsGKPermissions%3Atrue%3Bchallenge%3A0f7cfbf0-9c29-49b8-8d88-7d83fb0a3cb9%3B&scope=admin%3Apublic_key%2Crepo%2Cworkflow%2Cread%3Aorg%2Cuser%3Aemail&response_type=code&client_id=a7557949433b7d282a76>
2021-04-03 11:44:46 +0400

    update: reactive forms

--- intro-app/src/app/auth/sign-up/sign-up.component.ts
+++ intro-app/src/app/auth/sign-up/sign-up.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { SignUpData } from '../../data/sign-up-form.interface';
-import { NgForm } from '@angular/forms';
 import { FirebaseAuthService } from '../shared/firebase-auth.service';
 @Component({
   selector: 'app-sign-up',
@@ -9,15 +9,57 @@ import { FirebaseAuthService } from '../shared/firebase-auth.service';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-  signUpData: SignUpData = {
-    email: 'eve.holt@reqres.in',
-    password: 'pistol',
-  };
-  constructor(private fireAuthService: FirebaseAuthService) {}
+  email: FormControl;
+  password: FormControl;
+  signUpForm: FormGroup;
+  emailPattern: string = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
-  ngOnInit(): void {}
+  mouseOverSignUpButton: boolean = false;
 
-  onSubmit(signUpForm: NgForm) {
-    this.fireAuthService.signUp(this.signUpData);
+  constructor(public fireAuthService: FirebaseAuthService) {
+    this.email = new FormControl(
+      '',
+      Validators.compose([
+        Validators.required,
+        Validators.email,
+        Validators.pattern(this.emailPattern),
+      ])
+    );
+
+    this.password = new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]);
+
+    this.signUpForm = new FormGroup({
+      email: this.email,
+      password: this.password,
+    });
+  }
+
+  ngOnInit(): void {
+    // this.email.setValue('superman@yopmail.com');
+  }
+
+  validateEmail(): boolean {
+    return (
+      this.email.invalid && (this.email.touched || this.mouseOverSignUpButton)
+    );
+  }
+  validatePassword(): boolean {
+    return (
+      this.password.invalid &&
+      (this.password.touched || this.mouseOverSignUpButton)
+    );
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log('SUCCESS');
+      const signUpData: SignUpData = this.signUpForm.value;
+      this.fireAuthService.signUp(signUpData);
+    } else {
+      alert('ERROR');
+    }
   }
 }
